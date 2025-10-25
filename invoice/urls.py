13f1a8ff5d725557f3e1b47f29287e67e7938243
limon <mhlimon571@gmail.com>
2025-10-25 23:40:43 +0600

    combined single customer and edit

--- invoice/urls.py
+++ invoice/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create_customer/', views.create_customer, name='create_customer'),
+    path('all_customer/', views.all_customer, name='all_customer'),
+    path('single_customer/<int:pk>/', views.single_customer, name = 'single_customer'),
 ]
