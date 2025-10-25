2ae1fb763e26c72f1e292cb151d5153f5aa104cd
limon <mhlimon571@gmail.com>
2025-10-25 23:50:32 +0600

    add delete url

--- invoice/urls.py
+++ invoice/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create_customer/', views.create_customer, name='create_customer'),
     path('all_customer/', views.all_customer, name='all_customer'),
     path('single_customer/<int:pk>/', views.single_customer, name = 'single_customer'),
+    path('delete_customer/<int:pk>/', views.delete_customer, name='delete_customer'),
 ]
